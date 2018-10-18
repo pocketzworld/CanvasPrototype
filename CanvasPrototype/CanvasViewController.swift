5b52fe8b95ff948d13e6ff7b8b8ce8b111c1efd2
Josh Johnson <jnjosh@jnjosh.com>
2018-10-18 17:36:01 -0400

    fixes editing on launch

--- CanvasPrototype/CanvasViewController.swift
+++ CanvasPrototype/CanvasViewController.swift
@@ -96,6 +96,10 @@ class CanvasViewController: UIViewController {
           }
         }
       }
+        
+      if !isEditingCanvas {
+            layoutForNotEditing()
+      }
     }
   
     // MARK: Remote Data Updates
